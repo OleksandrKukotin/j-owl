4d5e5ee839d81cef9703de50c41d92738842a6b5
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2025-02-17 23:14:08 +0200

    Implemented fetching and parsing pages methods

--- src/main/java/com/github/oleksandrkukotin/jowl/crawler/PageParser.java
+++ src/main/java/com/github/oleksandrkukotin/jowl/crawler/PageParser.java
@@ -1,4 +1,24 @@
 package com.github.oleksandrkukotin.jowl.crawler;
 
+import org.jsoup.nodes.Document;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PageParser {
+
+    public PageParser() {}
+
+    public CrawledPage parsePage(String url, Document document) {
+        if (document == null) return null;
+
+        String title = document.title();
+        String content = document.body().text();
+        List<String> links= document.select("a[href]").stream()
+                .map(link -> link.absUrl("href"))
+                .filter(link -> !link.isEmpty())
+                .toList();
+
+        return new CrawledPage(url, title, content, links);
+    }
 }
--- src/main/java/com/github/oleksandrkukotin/jowl/crawler/WebCrawler.java
+++ src/main/java/com/github/oleksandrkukotin/jowl/crawler/WebCrawler.java
@@ -2,6 +2,7 @@ package com.github.oleksandrkukotin.jowl.crawler;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,28 +10,22 @@ public class WebCrawler {
 
     private static final Logger logger = LoggerFactory.getLogger(CrawlService.class);
 
-    private static final String TARGET_URL = "http://docs.oracle.com/javase/17/docs/api/";
     private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
             "(KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36";
 
     public WebCrawler() {
     }
 
-    public void crawl() {
+    public Document fetchPage(String url) {
         try {
-            Document doc = Jsoup.connect(TARGET_URL)
+            return Jsoup.connect(url)
                     .userAgent(USER_AGENT)
                     .timeout(10 * 1000)
                     .get();
-
-            logger.info(doc.title());
-            logger.info(String.valueOf(doc.body()));
         } catch (Exception e) {
             logger.error(e.getMessage());
+            return null;
         }
     }
 
-    public static void main(String[] args) {
-        new WebCrawler().crawl();
-    }
 }
