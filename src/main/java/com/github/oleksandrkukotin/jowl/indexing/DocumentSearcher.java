c0f55a605e0ab16ce85a58b08329192940725e15
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2025-03-02 23:01:24 +0200

    Refactored search method

--- src/main/java/com/github/oleksandrkukotin/jowl/indexing/DocumentSearcher.java
+++ src/main/java/com/github/oleksandrkukotin/jowl/indexing/DocumentSearcher.java
@@ -1,11 +1,12 @@
 package com.github.oleksandrkukotin.jowl.indexing;
 
+import com.github.oleksandrkukotin.jowl.exception.IndexSearchException;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.StoredFields;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
@@ -27,19 +28,20 @@ import java.util.Objects;
 public class DocumentSearcher {
 
     private static final Logger logger = LoggerFactory.getLogger(DocumentSearcher.class);
+    public static final String CONTENT_FIELD_NAME = "content";
 
     private final IndexSearcher indexSearcher;
     private final QueryParser queryParser;
+    private final Analyzer analyzer = new StandardAnalyzer();
 
     public DocumentSearcher(IndexSearcher indexSearcher) {
         this.indexSearcher = indexSearcher;
-        this.queryParser = new QueryParser("content", new StandardAnalyzer());
+        this.queryParser = new QueryParser(CONTENT_FIELD_NAME, new StandardAnalyzer());
     }
 
     public List<Document> search(String queryString, int maxResults) throws ParseException, IOException {
         logger.info("Executing search for query: '{}' with max results: {}", queryString, maxResults);
         Query query = queryParser.parse(queryString);
-        Analyzer analyzer = new StandardAnalyzer();
         TopDocs topDocs = indexSearcher.search(query, maxResults);
         StoredFields storedFields = indexSearcher.storedFields();
 
@@ -59,18 +61,18 @@ public class DocumentSearcher {
                 })
                 .filter(Objects::nonNull)
                 .map(doc -> {
-                    String text = doc.get("content");
-                    TokenStream stream = analyzer.tokenStream(text, new StringReader(text));
-                    String highlightedText = null;
-                    try {
-                        highlightedText = highlighter.getBestFragment(stream, text);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    } catch (InvalidTokenOffsetsException e) {
-                        throw new RuntimeException(e);
+                    String text = doc.get(CONTENT_FIELD_NAME);
+                    if (text != null && !text.isBlank()) {
+                        try (TokenStream stream = analyzer.tokenStream(CONTENT_FIELD_NAME, new StringReader(text))) {
+                            String highlightedText = highlighter.getBestFragment(stream, text);
+                            doc.add(new TextField("snippet", highlightedText, Field.Store.YES));
+                            doc.add(new TextField(CONTENT_FIELD_NAME, text, Field.Store.YES));
+                        } catch (IOException e) {
+                            throw new IndexSearchException(e.getMessage());
+                        } catch (InvalidTokenOffsetsException e) {
+                            logger.error("Highlighting failed for text {}", text, e);
+                        }
                     }
-                    doc.removeField("content");
-                    doc.add(new StringField("snippet", highlightedText, Field.Store.YES));
                     return doc;
                 })
                 .toList();
