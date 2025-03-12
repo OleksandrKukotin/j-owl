e8cdc17cceffed40657b27c5ff8e32c873a4d32e
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2025-03-12 23:32:59 +0200

    Changed JavadocPageParser logic and added some TODOs

--- src/main/java/com/github/oleksandrkukotin/jowl/crawler/CrawledPage.java
+++ src/main/java/com/github/oleksandrkukotin/jowl/crawler/CrawledPage.java
@@ -1,11 +1,11 @@
 package com.github.oleksandrkukotin.jowl.crawler;
 
-import java.util.List;
+import java.util.Set;
 
 public record CrawledPage(
         String url,
         String title,
         String content,
-        List<String> links
+        Set<String> links
 ) {
 }
--- src/main/java/com/github/oleksandrkukotin/jowl/crawler/javadoc/JavadocPageParser.java
+++ src/main/java/com/github/oleksandrkukotin/jowl/crawler/javadoc/JavadocPageParser.java
@@ -5,8 +5,9 @@ import com.github.oleksandrkukotin.jowl.crawler.PageParser;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class JavadocPageParser implements PageParser {
@@ -16,11 +17,13 @@ public class JavadocPageParser implements PageParser {
         if (document == null) return Optional.empty();
 
         String title = document.title();
+        // TODO: make the parser parse only useful content
         String content = document.body().text();
-        List<String> links = document.select("a[href]").stream()
+        // TODO: make the parser stay only on one domain
+        Set<String> links = document.select("a[href]").stream()
                 .map(link -> link.absUrl("href"))
-                .filter(link -> !link.isEmpty())
-                .toList();
+                .filter(link -> !link.isEmpty() && !link.contains("#"))
+                .collect(Collectors.toSet());
 
         return Optional.of(new CrawledPage(url, title, content, links));
     }
