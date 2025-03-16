b74f50ffa4aabe2c25f79afd30ea8eb91cab7f33
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2025-03-16 22:53:04 +0200

    Upgraded parsePage() method

--- src/main/java/com/github/oleksandrkukotin/jowl/crawler/javadoc/JavadocPageParser.java
+++ src/main/java/com/github/oleksandrkukotin/jowl/crawler/javadoc/JavadocPageParser.java
@@ -15,34 +15,18 @@ public class JavadocPageParser implements PageParser {
     public Optional<JavadocCrawledPage> parsePage(String url, Document document) {
         if (document == null) return Optional.empty();
 
-        String title = document.title();
-        // TODO: analyze and understand Javadoc page structure
-        // TODO: make the parser parse only useful content
-        String content = document.body().text();
-        // TODO: make the parser stay only on one domain
-        Set<String> links = document.select("a[href]").stream()
-                .map(link -> link.absUrl("href"))
-                .filter(link -> !link.isEmpty() && !link.contains("#"))
+        Optional<String> optionalTitle = Optional.of(document.select("h1").text());
+        String title = optionalTitle.orElse("");
+        String packageName = document.select("div.subTitle").next("package").text(); // Package name
+        String description = document.select("div.block").text(); // Class description
+
+        // TODO: Extract methods correctly
+        Set<String> methods = document.select("table.memberSummary tbody tr")
+                .stream()
+                .map(row -> row.select("td.colFirst a").text()) // Method name
+                .filter(name -> !name.isEmpty())
                 .collect(Collectors.toSet());
 
-        return Optional.of(new JavadocCrawledPage(url, title, content, links));
+        return Optional.of(new JavadocCrawledPage(url, title, packageName + "\n" + description, methods));
     }
-
-//    @Override
-//    public Optional<JavadocCrawledPage> parsePage(String url, Document document) {
-//        if (document == null) return Optional.empty();
-//
-//        String title = document.select("h1, h2").first().text(); // Class name
-//        String packageName = document.select("div.subTitle").text(); // Package name
-//        String description = document.select("div.block").text(); // Class description
-//
-//        // Extract methods
-//        Set<String> methods = document.select("table.memberSummary tbody tr")
-//                .stream()
-//                .map(row -> row.select("td.colFirst a").text()) // Method name
-//                .filter(name -> !name.isEmpty())
-//                .collect(Collectors.toSet());
-//
-//        return Optional.of(new JavadocCrawledPage(url, title, packageName + "\n" + description, methods));
-//    }
 }
