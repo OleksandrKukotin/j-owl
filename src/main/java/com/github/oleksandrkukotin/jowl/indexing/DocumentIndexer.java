732d43c94b11b1eae93254f0f17979a6a7d8f5da
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2025-03-19 22:21:03 +0200

    Refactored maxTermLength value in DocumentIndexer

--- src/main/java/com/github/oleksandrkukotin/jowl/indexing/DocumentIndexer.java
+++ src/main/java/com/github/oleksandrkukotin/jowl/indexing/DocumentIndexer.java
@@ -2,11 +2,13 @@ package com.github.oleksandrkukotin.jowl.indexing;
 
 import com.github.oleksandrkukotin.jowl.crawler.javadoc.JavadocCrawledPage;
 import com.github.oleksandrkukotin.jowl.crawler.javadoc.JavadocMethod;
+import jakarta.annotation.PostConstruct;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -16,9 +18,19 @@ import java.util.List;
 @Component
 public class DocumentIndexer {
 
-    private static final int MAX_TERM_LENGTH = 32766;
 
     private final IndexWriter indexWriter;
+
+    @Value("${lucene.index.max.term.length}")
+    private int maxTermLength;
+
+    @PostConstruct
+    private void validateMaxTermLength() {
+        if (maxTermLength <= 0) {
+            throw new IllegalArgumentException("The lucene.index.max.term.length property must be positive. " +
+                    "Current value:" + maxTermLength);
+        }
+    }
 
     public DocumentIndexer(IndexWriter indexWriter) {
         this.indexWriter = indexWriter;
@@ -43,9 +55,9 @@ public class DocumentIndexer {
         doc.add(new TextField("fullSignature", method.signature(), Field.Store.YES));
 
         String methodDescription = method.description();
-        if (methodDescription.length() > MAX_TERM_LENGTH) {
+        if (methodDescription.length() > maxTermLength) {
             for (String chunk : splitText(methodDescription)) {
-                doc.add(new TextField("chunk", chunk, Field.Store.YES));
+                doc.add(new TextField("description", chunk, Field.Store.YES));
             }
         } else {
             doc.add(new TextField("description", methodDescription, Field.Store.YES));
@@ -59,7 +71,7 @@ public class DocumentIndexer {
         doc.add(new TextField("className", page.className(), Field.Store.YES));
 
         String content = page.classDescription();
-        if (content.length() > MAX_TERM_LENGTH) {
+        if (content.length() > maxTermLength) {
             List<String> chunks = splitText(content);
             for (String chunk : chunks) {
                 doc.add(new TextField("classDescription", chunk, Field.Store.YES));
@@ -72,8 +84,8 @@ public class DocumentIndexer {
 
     private List<String> splitText(String text) {
         List<String> parts = new ArrayList<>();
-        for (int i = 0; i < text.length(); i += MAX_TERM_LENGTH) {
-            parts.add(text.substring(i, Math.min(i + MAX_TERM_LENGTH, text.length())));
+        for (int i = 0; i < text.length(); i += maxTermLength) {
+            parts.add(text.substring(i, Math.min(i + maxTermLength, text.length())));
         }
         return parts;
     }
