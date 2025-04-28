42e35a82f1896cd43e9d06a619acc148253be262
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2025-04-28 23:37:02 +0300

    Created Angular search service

--- j-owl-ui/src/app/services/search.service.ts
+++ j-owl-ui/src/app/services/search.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+export interface SearchResult {
+  name: string;
+  url: string;
+  snippet: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor() { }
+  private readonly API_URL = "http://localhost:8080/index/search"
+
+  constructor(private http: HttpClient) { }
+
+  search(query: string, maxResults: number = 10): Observable<SearchResult[]> {
+    return this.http.get<SearchResult[]>(`${this.API_URL}?query=${query}&maxResults=${maxResults}`);
+  }
 }
